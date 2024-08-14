230361471db9a5cc76caa022f3818e9669d9007a
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2024-08-14 16:00:21 +0300

    Install mui component library

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # react-router-workshop-august-2024
 Sirma Academy React Router Workshop
+
+
+## Steps
+1. Create github repo and clone
+2. Initial vite project
+3. Add ui component library
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Button from '@mui/material/Button';
+
 function App() {
     return (
         <>
             <h1>React Router Workshop</h1>
+            <Button variant="contained">Hello world</Button>
         </>
     )
 }
